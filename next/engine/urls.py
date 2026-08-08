9e1dbd5f3ee4f1857c0f95a15b4f1d0b1ef61457
Patrick Chen <plchen@madison.k12.wi.us>
2026-08-08 16:55:38 +0000

    Setting up Django environment

--- next/engine/urls.py
+++ next/engine/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from interface import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.login_view, name = "login")
 ]
